69d7b1c1ad41f84b9a78f25a7637e13e694a4814
Victor Salles <victor.s.salles@estudante.ufjf.br>
2024-12-23 23:34:08 -0300

    Inicio da preparação da main para receber os parametros

--- grafos/main.cpp
+++ grafos/main.cpp
@@ -1,9 +1,27 @@
-#include "Grafo_matriz.h"
 #include "Grafo_lista.h"
+#include "Grafo_matriz.h"
 
 using namespace std;
 
-int main() {
-    cout << "Hello, World!" << endl;
-    return 0;
+int main(int argc, char *argv[]) {
+
+  if (argc < 4) {
+    cerr << "Uso incorreto. Exemplos:\n"
+         << "  " << argv[0] << " -d -m grafo.txt\n"
+         << "  " << argv[0] << " -d -l grafo.txt\n"
+         << "  " << argv[0] << " -c -m descricao.txt grafo.txt\n"
+         << "  " << argv[0] << " -c -l descricao.txt grafo.txt\n";
+    return 1;
+  }
+
+  // Lê primeiro argumento: -d ou -c
+  string modo = argv[1]; // "-d" ou "-c"
+
+  // Lê segundo argumento: -m ou -l
+  string estrutura = argv[2]; // "-m" ou "-l"
+
+  // Ponteiro para a classe base (grafo)
+  Grafo *g = nullptr;
+
+  return 0;
 }
