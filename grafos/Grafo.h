dbf326069abcd9f1c1dc56d10edece15e2f6d276
Victor Salles <victor.salles@engenharia.ufjf.br>
2024-12-26 20:33:06 -0300

    Altera algumas definições do grafo.h

--- grafos/Grafo.h
+++ grafos/Grafo.h
@@ -1,35 +1,37 @@
 #ifndef GRAFO_H
 #define GRAFO_H
 
+#include <fstream>
 #include <iostream>
 #include <string>
-#include <fstream>
 
-class Grafo
-{
+class Grafo {
 protected:
-    int numNos = 0; // Indica o número de nós do grafo
-    bool direcionado = false; // Indica se o grafo é direcionado
-    bool arestas_ponderadas = false; // Indica se as arestas do grafo são ponderadas
-    bool vertices_ponderados = false; // Indica se os vértices do grafo são ponderados
+  int numNos = 0;           // Indica o número de nós do grafo
+  bool direcionado = false; // Indica se o grafo é direcionado
+  bool arestas_ponderadas =
+      false; // Indica se as arestas do grafo são ponderadas
+  bool vertices_ponderados =
+      false; // Indica se os vértices do grafo são ponderados
 
 public:
-    Grafo() = default;
-    virtual ~Grafo() = default;
-    virtual void imprimir();
-    virtual bool eh_bipartido();
-    virtual int n_conexo();
-    virtual int get_grau(int no);
-    virtual int get_ordem();
-    virtual bool eh_direcionado();
-    virtual bool vertice_ponderado();
-    virtual bool aresta_ponderada();
-    virtual bool eh_completo();
-    virtual bool eh_arvore();
-    virtual bool possui_articulacao();
-    virtual bool possui_ponte();
-    virtual Grafo* carrega_grafo(std::string &filename); // Não tenho certeza se o retorno esta correto
-    virtual Grafo* novo_grafo(std::string &filename); // Não tenho certeza se o retorno esta correto
+  Grafo() = default;
+  virtual ~Grafo() = default;
+  virtual void imprimir();
+  virtual bool eh_bipartido() = 0;
+  virtual int n_conexo() = 0;
+  virtual int get_grau() = 0;
+  virtual int get_ordem() = 0;
+  virtual bool eh_direcionado() = 0;
+  virtual bool vertice_ponderado() = 0;
+  virtual bool aresta_ponderada() = 0;
+  virtual bool eh_completo() = 0;
+  virtual bool eh_arvore() = 0;
+  virtual bool possui_articulacao() = 0;
+  virtual bool possui_ponte() = 0;
+  virtual void carrega_grafo(const std::string &arquivo) = 0;
+  virtual void novo_grafo(const std::string &descricao,
+                          const std::string &arquivoSaida) = 0;
 };
 
-#endif 
+#endif
